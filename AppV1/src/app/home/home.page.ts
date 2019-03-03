9fc5be18a17c2d560665858bf656b297e1b9350f
shedwreck <samuel.s.mullins@gmail.com>
2019-03-03 15:06:26 -0600

    Fresh Start

---
+++ AppV1/src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private keyboard: Keyboard) { }
+
+
+}
