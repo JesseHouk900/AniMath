121463fefca7274db5a834df54bfc440988edf5e
JesseHouk900 <jessejameshouk@gmail.com>
2019-04-10 22:27:30 -0500

    Fixed treeController log

--- Demo/js/LaTeXController/LaTexController.js
+++ Demo/js/LaTeXController/LaTexController.js
@@ -10,6 +10,7 @@ class LatexController {
             document.getElementById('LatexView').innerHTML =  "$${\\begin{align} " + tree[0].toTex() + " = " + tree[1].toTex() +  "\\end{align}}$$"
             console.log(document.getElementById('LatexView').innerHTML)
             window.treeController.tree = tree
+            console.log(treeController.tree)
         })
 
     }
