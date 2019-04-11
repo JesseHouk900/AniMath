5919c017bb463cce1306657eaff50655489b7120
JesseHouk900 <jessejameshouk@gmail.com>
2019-04-11 11:50:45 -0500

    started back on Transfer, trying to print parsed tree to bottom of screen

--- Demo/js/EquationParsing/Transfer.js
+++ Demo/js/EquationParsing/Transfer.js
@@ -8,24 +8,46 @@ var colors = ["#ff0000", "#ff4000", "#ff8000", "#ffbf00",
     "#0080ff", "#0040ff", "#0000ff", "#4000ff", "#4000ff",
     "#8000ff", "#bf00ff", "#ff00ff", "#ff00bf", "#ff0080",
     "#ff0040"]
-
-function Transfer(model) {
-    var equation = transfer(model.root, "", 0)
-    var term = prompt("Please enter the term that should be moved: " + equation, "2 * x")
-    var termTree = ParseSide(term)
+var ColorIndex = 0
+function Transfer(tree) {
+    ColorIndex = 0        
+    console.log(tree)
+    var left = transfer(tree[0], "", ColorIndex)
+    var right = transfer(tree[1], "", ColorIndex)
+    document.getElementById("OperatorOutput").innerHTML = left + " = " + right
+    var term = prompt("Please enter the term that should be moved: " + left + " = " + right , "2 * x")
+    
+    
     
 
 }
 
 function transfer(node, tE, colorIndex) {
-    if (node.value != null) {
+    if (node) {
+        console.log(tE)
+        var value
+        if (node.op) {
+            value = node.op
+        }
+        else if (node.value) {
+            value = node.value
+        }
+        else if (node.name) {
+            value = node.name
+        }
+        if (colorIndex > ColorIndex) {
+            ColorIndex = colorIndex
+        }
         tE += "(".fontcolor(colors[colorIndex])
         tE = transfer(node.left, tE, colorIndex + 1) 
         tE += node.value
         tE = transfer(node.right, tE, colorIndex + 2)
         tE += ")".fontcolor(colors[colorIndex])
-    }
-    return tE
+        
+        return tE
+
+    } 
+
 }
 
 function HandleTransfer(term) {
--- Demo/js/LaTeXController/LaTexController.js
+++ Demo/js/LaTeXController/LaTexController.js
@@ -7,7 +7,7 @@ class LatexController {
             var tree = mt.pieceTogether(lhs.value, rhs.value)
 
             //document.getElementById('LatexView').innerHTML =  "$${\\begin{align} " + tree[0].toTex() + " = " + tree[1].toTex() +  "\\end{align}}$$"
-            document.getElementById('LatexView').innerHTML =  "$${\\begin{align} " + tree[0].toTex() + " = " + tree[1].toTex() +  "\\end{align}}$$"
+            document.getElementById('LatexView').innerHTML =  "\\begin{align} " + tree[0].toTex() + " = " + tree[1].toTex() +  "\\end{align}"
             //console.log(document.getElementById('LatexView').innerHTML)
             window.treeController.tree = tree
             //console.log(treeController.tree)
